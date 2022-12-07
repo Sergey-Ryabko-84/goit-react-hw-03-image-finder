54ea0d5277bd89793e8f096d8146c4a5951aa56f
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-12-07 16:00:55 +0200

    adds update startup files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+
+export class App extends Component {
+  state = {}
+
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <div>React homework template</div>
+      </>
+    );
+  }
+};
